65f61f94b8e68ef194587bcaae8b87e06f80c95e
Pasin <mossps@Pasins-MacBook-Pro.local>
2024-01-19 23:00:39 +0700

    fire-base

--- server/Routes/product.js
+++ server/Routes/product.js
@@ -5,7 +5,31 @@ const { read, list, create, update, remove} = require('../Controllers/product.js
 
 const { auth } = require('../Middleware/auth')
 
-router.get('/product', list)
+const admin = require("../Config/firebase.js")
+const appCheck = admin.appCheck()
+const{ getAppCheck } = require ("firebase-admin/app-check");
+
+const appCheckVerification = async (req, res, next) => {
+    const appCheckToken = req.header("X-Firebase-AppCheck");
+
+    if (!appCheckToken) {
+        res.status(401);
+        return next("Unauthorized");
+    }
+
+    try {
+        const appCheckClaims = await getAppCheck().verifyToken(appCheckToken);
+
+        // If verifyToken() succeeds, continue with the next middleware
+        // function in the stack.
+        return next();
+    } catch (err) {
+        res.status(401);
+        return next("Unauthorized");
+    }
+} 
+
+router.get('/product', appCheckVerification, list)
 router.get('/product/:id',read)
 router.post('/product', create)
 router.put('/product/:id', update)
